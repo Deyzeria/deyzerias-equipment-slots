0507c8fd1f6019984268a8cb46d44424b8d0dde8
Sinantrarion <vikbar199@gmail.com>
2024-08-31 15:40:45 +0300

    Initial commit, full implementation

---
+++ config.mjs
@@ -0,0 +1,121 @@
+const ExtraEquipmentSlots = {};
+
+ExtraEquipmentSlots.Final = [];
+
+ExtraEquipmentSlots.DefaultEquipmentCategories = {
+  extraslots: "Extra Slots",
+  helmet: "Helmet",
+  gloves: "Gloves",
+  pants: "Pants",
+  boots: "Boots"
+}
+
+ExtraEquipmentSlots.ToProcess = [];
+
+ExtraEquipmentSlots.DefaultEquipmentSlots = {
+  clothhat: {
+    label: "Clothings Hat",
+    prof: true,
+    category: "helmet"
+  },
+  clothpants: {
+    label: "Clothing Pants",
+    prof: true,
+    category: "pants"
+  },
+  clothhands: {
+    label: "Clothing Gloves",
+    prof: true,
+    category: "gloves"
+  },
+  clothboots: {
+    label: "Clothing Boots",
+    prof: true,
+    category: "boots"
+  },
+
+  ring: {
+    label: "Ring",
+    prof: true,
+    category: "extraslots"
+  },
+  neck: {
+    label: "Necklace",
+    prof: true,
+    category: "extraslots"
+  },
+  face: {
+    label: "Face",
+    prof: true,
+    category: "extraslots"
+  },
+  back: {
+    label: "Back",
+    prof: true,
+    category: "extraslots"
+  },
+
+  lighthat: {
+    label: "Light Helmet",
+    prof: "lgt",
+    category: "helmet"
+  },
+  lightpants: {
+    label: "Light Pants",
+    prof: "lgt",
+    category: "pants"
+  },
+  lighthands: {
+    label: "Light Gloves",
+    prof: "lgt",
+    category: "gloves"
+  },
+  lightboots: {
+    label: "Light Boots",
+    prof: "lgt",
+    category: "boots"
+  },
+
+  mediumhat: {
+    label: "Medium Helmet",
+    prof: "med",
+    category: "helmet"
+  },
+  mediumpants: {
+    label: "Medium Pants",
+    prof: "med",
+    category: "pants"
+  },
+  mediumhands: {
+    label: "Medium Gloves",
+    prof: "med",
+    category: "gloves"
+  },
+  mediumboots: {
+    label: "Medium Boots",
+    prof: "med",
+    category: "boots"
+  },
+
+  heavyhat: {
+    label: "Heavy Helmet",
+    prof: "hvy",
+    category: "helmet"
+  },
+  heavypants: {
+    label: "Heavy Pants",
+    prof: "hvy",
+    category: "pants"
+  },
+  heavyhands: {
+    label: "Heavy Gloves",
+    prof: "hvy",
+    category: "gloves"
+  },
+  heavyboots: {
+    label: "Heavy Boots",
+    prof: "hvy",
+    category: "boots"
+  },
+}
+export default ExtraEquipmentSlots;
---
+++ deyzeria-equipment-slots.mjs
@@ -0,0 +1,85 @@
+import ExtraEquipmentSlots from './config.mjs';
+
+export class SetupArmorApi {
+  /**
+   * @param {Object} object 
+   */
+  SetupArmors(object) {
+    for (const [id, obj] of Object.entries(object)) {
+      CONFIG.DND5E.equipmentTypes[id] = obj.label;
+      CONFIG.DND5E.armorProficienciesMap[id] = obj.prof;
+
+      ExtraEquipmentSlots.ToProcess.push(
+        {
+          id: id,
+          label: obj.label,
+          category: obj.category
+        }
+      )
+    }
+  }
+}
+
+Hooks.once("init", () => {
+  const setupArmor = new SetupArmorApi;
+  globalThis.Deyzeria ||= {};
+  globalThis.Deyzeria.SetupArmors = setupArmor.SetupArmors;
+
+  CONFIG.Deyzeria ||= {};
+  CONFIG.Deyzeria.ExtraEquipmentSlotsCategories = ExtraEquipmentSlots.DefaultEquipmentCategories;
+
+  setupArmor.SetupArmors(ExtraEquipmentSlots.DefaultEquipmentSlots);
+});
+
+Hooks.once("setup", () => {
+  ExtraEquipmentSlots.Final = SetupFinalForVisual();
+});
+
+// Setups ExtraEquipmentSlots.Final for the further visual display
+function SetupFinalForVisual() {
+  const toProcess = ExtraEquipmentSlots.ToProcess;
+  const categories = ExtraEquipmentSlots.DefaultEquipmentCategories;
+  const final = [];
+
+  for (const [id, label] of Object.entries(categories)) {
+    var typeUnfiltered = toProcess.filter(elem => elem.category === id);
+    var typesFiltered = {};
+    typeUnfiltered.forEach(element => {
+      typesFiltered[element.id] = element.label
+    });
+
+    final.push({
+      label: label,
+      types: typesFiltered
+    });
+  }
+
+  return final;
+}
+
+Hooks.on("renderItemSheet5e", (app, html, data) => {
+  if (data.item.type == 'equipment') {
+    SetupArmorChoice(html, data);
+  }
+});
+
+// Default Item Sheet
+function SetupArmorChoice(html, data) {
+  if (!html.hasClass("tidy5e-sheet")) {
+    var choiceList = html.find(".details").find('.form-group [name="system.type.value"]');
+    ExtraEquipmentSlots.Final.forEach(element => {
+      let choices = $(`<optgroup label="${element.label}"></optgroup>`);
+      for (const [armor, name] of Object.entries(element.types)) {
+        choices.append(`<option value="${armor}" ${data.system.type.value == armor ? 'selected' : ''}>${name}</option>`);
+      }
+      choiceList.append(choices);
+    });
+  }
+}
+
+// Tidy item sheet
+Hooks.once('tidy5e-sheet.ready', (api) => {
+  ExtraEquipmentSlots.Final.forEach((group) => {
+    api.config.item.registerCustomEquipmentTypeGroup(group);
+  });
+});
